19af799b534e88e78a69bc9d1f90276b2e9d67b0
guhe <helmut.guggenbichler@skidata.com>
2019-02-18 13:33:19 +0100

    new

--- maven-art-15-deployment-remote/src/main/java/de/art/maven/examples/site/Main.java
+++ maven-art-15-deployment-remote/src/main/java/de/art/maven/examples/site/Main.java
@@ -14,6 +14,7 @@ public class Main {
     public static void main(String[] args) {
         BasicConfigurator.configure();
         //TODO: nothing to do
+		//123
         LOG.warn("Hello World!!!");
     }
 }
